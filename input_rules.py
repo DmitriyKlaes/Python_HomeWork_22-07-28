586df01eeb1ac4ca7f2ce92499d6186068234821
DmitriyKlaes <89119584059@mail.ru>
2022-08-22 16:21:16 +0300

    Телефонный справочник

--- input_rules.py
+++ input_rules.py
@@ -2,4 +2,12 @@ def correct_value(message, length = 2):
     value = input(message)
     while not value.isdigit() or not 0 < int(value) <= length:
         value = input(f'Неверный ввод. Введите соответствующий номер: ')
-    return value
+    return int(value)
+
+
+def default_value(string):
+    temp_string = string.replace(' ', '')
+    if temp_string == '':
+        return '(отсутствует)'
+    else:
+        return string
